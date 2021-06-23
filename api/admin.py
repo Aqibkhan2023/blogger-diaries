0d12c375fa23d9498853fa5af50d0664a6f394ba
Fizza-Rubab <fr06161@st.habib.edu.pk>
2021-06-24 00:49:18 +0500

    registering sites and admin customization

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Blog, Comment, Image, Category
+admin.register(Comment, Image, Category)(admin.ModelAdmin)
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'authorName', 'publishedAt']
+    list_filter = ['createdAt', 'publishedAt', 'title', 'authorName']
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug':('title',)}
+    date_hierarchy = 'publishedAt'
+
