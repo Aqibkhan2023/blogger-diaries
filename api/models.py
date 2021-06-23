06b295364645e2962e35014e62a97dfe602148e0
Fizza-Rubab <fr06161@st.habib.edu.pk>
2021-06-24 00:49:40 +0500

    models corrected and default provided

--- api/models.py
+++ api/models.py
@@ -1,37 +1,45 @@
 from django.db import models
 
-# Create your models here.
-class BlogModel(models.Model):
-    title = models.CharField(max_length = 200)
+
+class Category(models.Model):
+    title = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.title
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=250)
     content = models.TextField()
     slug = models.SlugField(max_length=1000)
-    image = models.ImageField()
     createdAt = models.DateTimeField(auto_now_add=True)
     lastEdited = models.DateTimeField(auto_now=True)
+    publishedAt = models.DateTimeField(blank=True, null=True)
     authorName = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ("-publishedAt",)
+
     def __str__(self):
         return f'Title: {self.title} Author: {self.authorName}'
+
 
 class Comment(models.Model):
     content = models.TextField()
     userName = models.CharField(max_length=100)
     timePosted = models.DateTimeField(auto_now_add=True)
-    blogId = models.ForeignKey('BlogModel', on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
     def __str__(self):
         return f'Comment Author: {self.userName}'
 
-class Category(models.Model):
-    title:models.CharField(max_length = 200)
-    def __str__(self):
-        return self.title
+
 class Image(models.Model):
-    name: models.CharField(max_length=200)
-    imageData: models.TextField()
-    imageType: models.CharField(max_length=10)
-    blogId: models.ForeignKey('BlogModel', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    imageData = models.TextField()
+    imageType = models.CharField(max_length=10)
+    blog = models.ForeignKey(Blog, default=None, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
-
-
-
-
