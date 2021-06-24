8b61fba9a706508b114912bce902b66ffc005663
Fizza-Rubab <fr06161@st.habib.edu.pk>
2021-06-25 03:34:44 +0500

    create method defined for blog and comment serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,38 +1,61 @@
 from rest_framework import serializers
 from . import models
+from django.utils.text import slugify
 
-class BlogSerializer(serializers.Serializer):
-    model = models.Blog
-    fields = [
-        'title',
-        'slug',
-        'authorName',
-        'content',
-        'category',
-        'createdAt',
-        'publishedAt',
-        'lastEdited'        
-    ]
+class BlogSerializer(serializers.ModelSerializer):
+    # category = serializers.RelatedField(read_only=True)
 
-class CommentSerializer(serializers.Serializer):
-    model = models.Comment
-    fields = [
-        'userName',
-        'content',
-        'timePosted',
-        'blog'        
-    ]
-class CategorySerializer(serializers.Serializer):
-    model = models.Category
-    fields = [
-        'title',  
-    ]
+    class Meta:
+        model = models.Blog
+        fields = (
+            'title',
+            'authorName',
+            'content',
+            'category',
+            'slug',
+            'createdAt',
+            'publishedAt',
+            'lastEdited'        
+        )
+    def create(self, validated_data):
+        return models.Blog.objects.create(**validated_data)
 
-class ImageSerializer(serializers.Serializer):
-    model = models.Image
-    fields = [
-        '`name`',
-        'imageData',
-        'imageType',
-        'blog'  
-    ]
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.content = validated_data.get('content', instance.content)
+        instance.lastEdited = validated_data.get('lastEdited', instance.lastEdited)
+        instance.publishedAt = validated_data.get('publishedAt', instance.publishedAt)
+        instance.authorName = validated_data.get('authorName', instance.authorName)
+        instance.category = validated_data.get('category', instance.category)
+        instance.slug = slugify(instance.title)
+        instance.save()
+        return instance
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Comment
+        fields = (
+            'userName',
+            'content',
+            'timePosted',
+            'blog'        
+        )
+    def create(self, validated_data):
+        return models.Comment.objects.create(**validated_data)
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Category
+        fields = (
+            'title',  
+        )
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Image
+        fields = (
+            'name',
+            'imageData',
+            'imageType',
+            'blog'  
+        )
