6327d62876a5ff613bb436f9ac976c8d40cd2b11
Fizza-Rubab <fr06161@st.habib.edu.pk>
2021-06-25 03:33:52 +0500

    paths added for blog and comment

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from froala_editor import views
+from api.api import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('froala_editor/', include('froala_editor.urls')),
+    path('blogs/', BlogView.as_view(), name='blogs',),
+    path('blogs/<int:id>', BlogDetailView.as_view(), name = 'blog_details'),
+    path('comments/<int:blog_id>', CommentView.as_view(), name='comments',),
+    # path('comments/<int:id>', CommentDetailView.as_view(), name = 'comment_details'),
 ]
